798944560ad133510ab6276d56bee63a0ec558a4
MatthewM60 <112290489+MatthewM60@users.noreply.github.com>
2025-02-19 01:40:33 -0800

    Fixed Merge Conflicts: Tag Shifting

--- Assets/src/Benton/Bullet.cs
+++ Assets/src/Benton/Bullet.cs
@@ -3,22 +3,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage; // Damage the bullet deals
-    public string targetTag; // The target object that is hit
     public string targetTag = "Enemy"; // The target object that is hit
     public string obstacleTag = "Walls";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Check if the bullet hits an target
-        if (collision.CompareTag(targetTag))
-        {
-            // EnemyHealth health = collision.GetComponent<EnemyHealth>();
-            // if (health != null)
-            // {
-            //     //health.TakeDamage(damage); // Deal damage to the enemy
-            // }
-
-        // Check if the bullet hits an enemy
         if (collision.CompareTag(targetTag)){
             EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
             if (enemyHealth != null){
