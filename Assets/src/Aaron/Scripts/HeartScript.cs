d17bec9faeecfb9300732b43612a4c0c3acc9c77
aaron-guevara <siddhartha6@icloud.com>
2025-02-23 22:49:06 -0800

    Player health-aaron

---
+++ Assets/src/Aaron/Scripts/HeartScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartScript : MonoBehaviour
+{
+
+    public PlayerHealth playerHealth;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+     for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < playerHealth.GetHealth())
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < playerHealth.maxHealth)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+}
+}
