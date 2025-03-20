d2907a21d645e568e56eb3bd04b3d1c4462406c9
B3nton-Wilson <wils8062@vandals.uidaho.edu>
2025-03-20 09:20:58 -0700

    Added a more clear label and made it so crosshair spawns back in pause

--- Assets/src/Benton/CrossHair.cs
+++ Assets/src/Benton/CrossHair.cs
@@ -12,4 +12,18 @@ public class MouseCursor : MonoBehaviour
         // Optionally, hide the default system cursor
     }
 
+    void Update()
+    {
+       if (Time.timeScale == 0)
+       {
+           // Show the cursor when the game is paused
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+       }
+       else
+       {
+           // Hide the cursor when the game is not paused
+            Cursor.SetCursor(crosshairTexture, Vector2.zero, CursorMode.ForceSoftware);
+       }
+    }
+
 }
